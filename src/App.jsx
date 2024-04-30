ff093be357547c3d0a3b0e0413d0ca064b95b3c9
Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>
2024-04-30 20:17:11 +0200

    feat: file cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 function App() {
-  console.log({ meta: import.meta.env.VITE_API_BASE_URL });
   return (
     <>
-      <h1>{import.meta.env.VITE_API_BASE_URL}</h1>
+      <h1>Notekeeper App</h1>
     </>
   );
 }
