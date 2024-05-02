0fd507c24d47f90b033aa0f98a0a182c7b4ed35d
Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>
2024-05-03 00:09:45 +0200

    refactor: function segregation

--- src/helpers/validation/validateType.js
+++ src/helpers/validation/validateType.js
@@ -1,16 +1,12 @@
 export const validateType = (note) => {
   const isValidType = (value, type) => typeof value === type;
-  const isEmpty = (value) => value === "";
-
-  const isValid = (value, type) =>
-    isValidType(value, type) && !isEmpty(value);
 
   const result =
-    isValid(note.name, "string") &&
-    isValid(note.description, "string") &&
-    isValid(note.important, "boolean") &&
-    isValid(note.status, "string") &&
-    isValid(note.due_date, "string");
+    isValidType(note.name, "string") &&
+    isValidType(note.description, "string") &&
+    isValidType(note.important, "boolean") &&
+    isValidType(note.status, "string") &&
+    isValidType(note.due_date, "string");
 
   return result;
 };
