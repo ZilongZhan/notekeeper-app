1f49ee3d7f32395a9220d90dcd4cf76ed776fe57
Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>
2024-05-13 01:12:34 +0200

    feat: initial app view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { NotesList, CreateNoteForm } from "./components";
+import { useNotes } from "./hooks";
+
 function App() {
+  const { notes, handleSetNotes } = useNotes();
+
   return (
     <>
       <h1>Notekeeper App</h1>
+      <CreateNoteForm notes={notes} handleSetNotes={handleSetNotes} />
+      <NotesList notes={notes} handleSetNotes={handleSetNotes} />
     </>
   );
 }
